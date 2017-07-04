09fc5e4b16959ffab0fe0631f6a82174fc26e456
Navya Basavaraju <navsie@wlan-141-23-169-80.tubit.tu-berlin.de>
2017-07-04 17:58:39 +0200

    Creatign stack for assignment 3

--- Assignment3/assignment3-resources/create-stack.sh
+++ Assignment3/assignment3-resources/create-stack.sh
@@ -5,4 +5,4 @@ test $# = 1 || { echo "Need 1 parameter: Name of the stack to create"; exit 1; }
 STACK="$1"
 
 # Create the stack using server-landscape.yaml and defining all necessary parameters
-[[TODO]]
+openstack stack create --wait --template server-landscape.yaml --parameter "external_net=tu-internal;key_pair=cloudgroup02;flavor=610f44b0-d25a-44bc-a6b1-8b22e68675e5;image=11f6b8aa-31df-4b66-8b42-5ee9760c47ba;zone=Cloud Computing 2017" $STACK
