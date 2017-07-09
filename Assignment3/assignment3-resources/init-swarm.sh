5838db7bb9c74677f985a5c20e81c6d1f9e0877e
Ubuntu <Seema-n88>
2017-07-09 15:15:17 +0000

    Frontend DOckerfile and init-swarm

--- Assignment3/assignment3-resources/init-swarm.sh
+++ Assignment3/assignment3-resources/init-swarm.sh
@@ -7,12 +7,12 @@ STACK="$1"
 # Obtain information from OpenStack. It is important that the last two variables are named LC_* (see last comment in this script).
 # The three variables correspond to output variables of the server-landscape.yaml template.
 echo "Obtainining information about stack ${STACK}..."
-export MASTER_FLOATING=$([[TODO]])
-export LC_MASTER_PRIVATE=$([[TODO]])
-export LC_BACKEND_IPS=$([[TODO]])
+export MASTER_FLOATING=$(openstack stack output show CCG2_A3_Stack floating_ip -f value | tail -n +3)
+export LC_MASTER_PRIVATE=$(openstack stack output show CCG2_A3_Stack private_ip -f value | tail -n +3)
+export LC_BACKEND_IPS=$(openstack stack output show CCG2_A3_Stack backend_ips -f value | tail -n +3 | jq -r ".| @tsv")
 
 # Copy both docker-compose files to the frontend server
-[[TODO]]
+cp  Frontend/docker-compose.yml Backend/docker-compose.yml /etc
 
 # Define a multi-line variable containing the script to be executed on the frontend machine.
 # The tasks of this script:
@@ -26,21 +26,21 @@ read -d '' INIT_SCRIPT <<'xxxxxxxxxxxxxxxxx'
 sudo docker ps &> /dev/null || sudo service docker restart
 
 # Initialize the Docker swarm
-sudo [[TODO]]
+sudo docker swarm init --advertise-addr $LC_MASTER_PRIVATE
 
 # Make sure the SSH connection to the backend servers works without user interaction
 SSHOPTS="-o StrictHostKeyChecking=no -o ConnectTimeout=3 -o BatchMode=yes"
 ssh-keyscan $LC_BACKEND_IPS > ~/.ssh/known_hosts
 
 # Obtain a token that can be used to join the swarm as a worker
-TOKEN=$(sudo docker [[TODO]])
+TOKEN=$(sudo docker swarm join-token worker)
 
 # Prepare the script to execute on the backends to join the docker swarm.
 # First make sure that docker is running properly...
 backend_setup_1="{ sudo docker ps &> /dev/null || sudo service docker restart; }"
 
 # ... then join the docker swarm on the frontend server
-backend_setup_2="sudo docker [[TODO]]"
+backend_setup_2="sudo docker swarm join --token $TOKEN $LC_MASTER_PRIVATE:2377"
 
 # Connect to the backend servers and make them join the swarm
 for i in $LC_BACKEND_IPS; do ssh $SSHOPTS ubuntu@$i "$backend_setup_1 && $backup_setup_2"; done
